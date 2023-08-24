a812e905b9a3fc90fde16da545ba98ee613649b0
Andres Molina Ospina <afmo2@byu.edu>
2023-08-24 12:11:26 -0600

    Plus button added to home. Still needs functionality

--- app/(tabs)/home/index.jsx
+++ app/(tabs)/home/index.jsx
@@ -1,6 +1,7 @@
-import { useRouter, Stack  } from "expo-router";
-import {SafeAreaView, ScrollView, TouchableOpacity, Text} from "react-native";
+import { useRouter, Stack } from "expo-router";
+import { SafeAreaView, ScrollView, View, TouchableOpacity, Text } from "react-native";
 import { AuthStore, appSignOut } from "../../../store";
+import { PlusIcon } from "react-native-heroicons/solid";
 
 function Index() {
     const router = useRouter();
@@ -16,7 +17,7 @@ function Index() {
     }
 
     return (
-        <SafeAreaView className={'flex-1 bg-secondary'}>
+        <SafeAreaView className={'relative flex-1 bg-secondary'}>
             <ScrollView>
                 <TouchableOpacity onPress={handleLogOut}>
                     <Text>
@@ -24,6 +25,17 @@ function Index() {
                     </Text>
                 </TouchableOpacity>
             </ScrollView>
+
+            {
+                AuthStore.getRawState().data?.isCompany ?
+                    <TouchableOpacity className={`absolute w-14 h-14 bottom-20 right-5 bg-primary flex-none justify-center items-center 
+                                                rounded-full shadow-sm shadow-inner shadow-slate-600`}>
+                        <PlusIcon color={'#9BC8E3'} size={40} />
+                    </TouchableOpacity>
+                    :
+                    null
+            }
+
         </SafeAreaView>
     )
 }
